346b81837a232a8dfc6665444f0b95dab19afce6
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-10-07 20:16:12 +0100

    Got datalen value and best effort value

--- js/global.js
+++ js/global.js
@@ -1,5 +1,9 @@
 // -------------------- START UP --------------------
 
+// Hide error message pop up
+// WORKING
+$('#error-message').hide();
+
 // Hide subscriber settings on start
 // WORKING
 $('#subscriber-settings').hide();
@@ -13,6 +17,51 @@ $('#instances-setting').hide();
 $('#multicastAddress-setting').hide();
 
 // ----------------- END OF START UP ----------------
+
+// MAIN SCRIPT CREATION FUNCTION
+// Not working
+// CHANGE IT BACK TO #createButton AFTER IT IS WORKING
+
+$('#reset-button').on('click', () => {
+    /*
+        For all checkboxes:
+        FALSE = close-circle-outline
+        TRUE = checkmark-circle-outline
+    */
+
+    // Get BestEffort Value
+    let BestEffort = document.querySelector('#bestEffort'); 
+    let bestEffortValue;
+    BestEffort.name === 'checkmark-circle-outline' ? bestEffortValue = '-bestEffort' : bestEffortValue = '';
+
+    // Get DataLen Value
+    // If DataLen is empty set it to 100.
+    let dataLen = document.querySelector('#dataLen');
+    let dataLenValue;
+    dataLen.value === '' ? dataLenValue = '-dataLen 100' : dataLenValue = `-dataLen ${dataLen.value}`;
+
+    if(dataLen.value < 28 && dataLen.value !== ''){
+        showError("Data Length can't be less than 28.");
+    }else if(dataLen.value > 2147483128){
+        showError("Data Length can't be more than 2147483128.");
+    }
+
+    console.log(`dataLenValue: ${dataLenValue}`);
+
+});
+
+// Close error message if opened
+// WORKING
+$('#error-message').on('click', () => {
+    
+    var error = document.querySelector('#error-message');
+    
+    while(error.firstChild){
+        error.removeChild(error.firstChild);
+    }
+
+    $('#error-message').hide();
+});
 
 // Show multicast address when multicast is clicked
 // WORKING
@@ -61,3 +110,14 @@ $('#type-select').on('change', (e) => {
     }
 
 });
+
+// -------------------- FUNCTION DEFINITIONS --------------------
+
+function showError(errorMessage){
+    var errorMessagePopUp = document.querySelector('#error-message');
+    var errorText = document.createTextNode(errorMessage);;
+
+    errorMessagePopUp.appendChild(errorText);
+
+    $('#error-message').show();
+}
