e8082556cb312134d44a2d15519c8e0c1349657d
Kaleem <Kaleem.peeroo@city.ac.uk>
2020-01-27 16:03:33 +0000

    Quick create general settings now updates its file value when changed

--- js/quickCreateGeneralSettings.js
+++ js/quickCreateGeneralSettings.js
@@ -1,9 +1,7 @@
 let quickCreateGenSettingVals = readData( path.join(__dirname, '../data/QuickCreateGeneralSettings.json') );
-
 let outputContainer = document.querySelector('#quick-create-general-settings-container');
 
-console.log(outputContainer);
-
+// Render inputs from quick create general settings file
 quickCreateGenSettingVals.forEach(item => {
 
     let divDOM = document.createElement('div');
@@ -26,8 +24,13 @@ quickCreateGenSettingVals.forEach(item => {
         inputDOM.id = item.id;
 
         inputDOM.className = 'checkmark';
+
+        inputDOM.addEventListener('click', updateSetting);
+        
     }else if(item.type == 'dropdown'){
         inputDOM = document.createElement('select');
+
+        inputDOM.id = item.id;
 
         item.options.forEach(option => {
             let optionDOM = document.createElement('option');
@@ -41,6 +44,8 @@ quickCreateGenSettingVals.forEach(item => {
             inputDOM.appendChild(optionDOM);
         });
 
+        inputDOM.addEventListener('change', updateSetting);
+
     }else if(item.type == 'input'){
         inputDOM = document.createElement('input');
 
@@ -49,11 +54,70 @@ quickCreateGenSettingVals.forEach(item => {
         inputDOM.id = item.id;
 
         inputDOM.value = item.value;
+
+        inputDOM.addEventListener('keyup', updateSetting);
+    }else if(item.type == 'file'){
+        inputDOM = document.createElement('input');
+        inputDOM.type = 'file';
+        inputDOM.id = item.id;
+
+        inputDOM.addEventListener('change', updateSetting);
     }
 
     divDOM.appendChild(pDOM);
     divDOM.appendChild(inputDOM);
 
     outputContainer.appendChild(divDOM);
-
 });
+
+function updateSetting(e){
+    let eTag = e.target.tagName;
+    let eId = e.target.id;
+    let eType;
+    let settingValue;
+    let settingIndex;
+
+    let allSettings = JSON.parse(fs.readFileSync( path.join( __dirname, '../data/QuickCreateGeneralSettings.json' ) ));
+
+    allSettings.forEach((setting, index) => {
+        if(setting.id == eId){
+            settingValue =  setting;
+            settingIndex = index;
+        }
+    });
+
+    if(eTag.toLowerCase() == 'ion-icon'){
+        
+        if(e.target.name == 'close-circle-outline'){
+            e.target.name = 'checkmark-circle-outline';
+            settingValue.value = true;
+        }else{
+            e.target.name = 'close-circle-outline';
+            settingValue = false;
+        }
+
+    }else if(eTag.toLowerCase() == 'select'){
+
+    }else if(eTag.toLowerCase() == 'input'){
+
+        eType = e.target.type;
+
+        if(eType.toLowerCase() == 'number'){
+            
+            settingValue.value = e.target.value;
+
+        }else if(eType.toLowerCase() == 'file'){
+
+            settingValue.value = e.target.files[0].path;
+
+        }
+    }
+
+    fs.writeFile(path.join( __dirname, '../data/QuickCreateGeneralSettings.json' ), JSON.stringify(allSettings), err => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Updated QuickCreateGeneralSettings.json!');
+        }
+    });
+}
